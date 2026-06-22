2f0b09e87b7f50dbc160bf2d84819dc7b358a104
Raul Lize Teixeira <lizeraul1@gmail.com>
2026-06-22 00:39:20 -0300

    chore: remove unused SlugAlreadyInUseError file

--- backend/src/domain/content/errors/slug-already-in-use.error.ts
+++
@@ -1,7 +0,0 @@
-import { AppError } from '../../../shared/core/errors/app-error';
-
-export class SlugAlreadyInUseError extends AppError {
-  constructor(slug: string) {
-    super(`O slug '${slug}' já está em uso.`, 400);
-  }
-}
