f754d7b9177fd02232ed2eb73fcda796fac99b3d
Raul Lize Teixeira <lizeraul1@gmail.com>
2026-07-10 17:08:19 -0300

    docs: clean up environment variable loading in functional tests

--- backend/__functional_tests__/src/functionalTestHelper.js
+++ backend/__functional_tests__/src/functionalTestHelper.js
@@ -2,12 +2,9 @@ const path = require("node:path");
 const dotenv = require("dotenv");
 const request = require("supertest");
 
-// Carrega variaveis do arquivo .env local do __functional_tests__ primeiro para ter precedencia
 dotenv.config({
     path: path.resolve(__dirname, "../.env")
 });
-
-// Carrega variaveis do arquivo .env da raiz do backend apenas para preencher o que estiver faltando
 dotenv.config({
     path: path.resolve(__dirname, "../../.env")
 });
@@ -17,11 +14,8 @@ function loadFixtures() {
 }
 
 const fixtures = loadFixtures();
-
-// O port padrao do AprovaAI e 3001
 const applicationBaseUrl = process.env.APPLICATION_BASE_URL || fixtures.application.baseUrl;
 
-// Resolucao absoluta do PrismaClient do backend para evitar problemas de inicializacao local
 const prismaClientPath = path.resolve(__dirname, "../../node_modules/@prisma/client");
 const { PrismaClient } = require(prismaClientPath);
 const prisma = new PrismaClient();
